7539f65df2993299c247b1473c2e09b8e276089f
Trever O <toveson9522@gmail.com>
2020-11-07 20:24:36 -0700

    first question is now being asked

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table')
 
-
+// mySQL connection
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 8080,
@@ -12,12 +12,50 @@ const connection = mysql.createConnection({
     database: 'EmployeeManagment_DB'
 });
 
+// connectiong to mySQL
 connection.connect(function(err) {
     if (err) throw err;
+    console.log(err);
 });
 
 // CLI stuff
+function start () {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'firstChoice',
+            message: 'What do you want to do?',
+            choices: [
+                // add
+                'Add depmartment, role or employee',
+                
+                // view
+                // bonus View employess by manager
+                // View total salary of all department members (one total not each person in the department)
+                'View depmartment, role or employee',
+                
+                // update
+                // bonus Update employee managers
+                'Update depmartment, role or employee'
 
+                // bonus
+                // delete department, role employee
+            ]
+        }
+    ]).then(function (userChoice) {
+                switch (userChoice.action) {
+                    case 'Add depmartment, role or employee':
+                        // function to add
+                        break;
+                    case 'View depmartment, role or employee':
+                        // function to view
+                        break;
+                    case 'Update depmartment, role or employee':
+                        // function to update
+                        break;
+                }
+            });
+        }
 
 // needed
 
@@ -37,5 +75,8 @@ connection.connect(function(err) {
 // View employess by manager
 // Delete department
 // Delete role
-// Delete employess
+// Delete employees
 // View total salary of all department members (one total not each person in the department)
+
+// start the questions!!!!
+start();
