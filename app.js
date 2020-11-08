72939bc7e100157bb4ea9ffc84e2ff47a6260649
Trever O <toveson9522@gmail.com>
2020-11-07 18:46:41 -0700

    added pseudo code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const cTable = require('console.table')
 
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 8080,
@@ -10,3 +11,31 @@ const connection = mysql.createConnection({
     password: 'Clear2020',
     database: 'EmployeeManagment_DB'
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+// CLI stuff
+
+
+// needed
+
+// Add department
+// Add roles
+// Add employees
+// View department
+// View roles
+// View employees
+// update department
+// update roles
+// update employees
+
+// bonus stuff
+
+// Update employee managers
+// View employess by manager
+// Delete department
+// Delete role
+// Delete employess
+// View total salary of all department members (one total not each person in the department)
