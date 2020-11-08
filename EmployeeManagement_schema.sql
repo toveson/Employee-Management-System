8e6e6fc38b001d1cbb91d480d5c83a236eeb0a73
Trever O <toveson9522@gmail.com>
2020-11-07 19:24:42 -0700

    added schema and seeds for data base

--- EmployeeManagement_schema.sql
+++ EmployeeManagement_schema.sql
@@ -4,13 +4,13 @@ CREATE database EmployeeManagment_DB;
 USE EmployeeManagment_DB;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+   id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL  NOT NULL,
     department_id INT  NOT NULL,
@@ -18,9 +18,9 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30)  NOT NULL,
-    lastname VARCHAR(30)  NOT NULL,
+    last_name VARCHAR(30)  NOT NULL,
     role_id INT  NOT NULL,
     manager_id INT,
     PRIMARY KEY (id)
